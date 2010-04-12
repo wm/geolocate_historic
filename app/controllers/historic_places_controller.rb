9da94bb3ba1c971c3bbac6c9c1abec1ab5fc4eb7
Will Mernagh <wmernagh@gmail.com>
2010-04-11 23:04:32 -0400

    Changinf SQL to work with pgSQL

--- app/controllers/historic_places_controller.rb
+++ app/controllers/historic_places_controller.rb
@@ -7,14 +7,7 @@ class HistoricPlacesController < ApplicationController
     
     if params[:lat] && params[:lng]
       distance_calculation = distance_calc(params[:lat],params[:lng])
-     
-      # Had to do it this way for PGsql. Heroku only has PGsql. 
-      # This will also work for MySQL.
-      @historic_places = HistoricPlace.all(
-        :select => "id,title,lat,lng,#{distance_calculation} as calc", 
-        :group => "id,title,lat,lng HAVING calc <= #{DISTANCE}", 
-        :order => "calc"
-      )
+      @historic_places = HistoricPlace.find_by_sql(distance_calculation)
     end
 
     respond_to do |format|
@@ -38,9 +31,13 @@ class HistoricPlacesController < ApplicationController
   # Returns a string to be used by SQL for calculatin the distance between the
   # point entered and the lat, lng in the database row
   #
-  def distance_calc(lat,lng)
+  def distance_calc(lat,lng)    
+    # Had to do it this way for PGsql. Heroku only has PGsql. 
+    # This will also work for MySQL.
     calc = "((ACOS(SIN(#{lat} * PI() / 180) * SIN(lat * PI() / 180) + "
     calc = calc + "COS(#{lat} * PI() / 180) * COS(lat * PI() / 180) * "
-    calc = calc + "COS((#{lng} - lng) * PI() / 180)) * 180 / PI()) * 60 * 1.1515)"
+    calc = calc + "COS((#{lng} - lng) * PI() / 180)) * 180 / PI()) * 60 * 1.1515)" 
+    calc = "SELECT id,title,lat,lng,#{calc} as calc FROM `historic_places` "
+    calc = calc + "GROUP BY id,title,lat,lng HAVING calc <= 2 ORDER BY calc"
   end
 end
